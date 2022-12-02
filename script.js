d88abe105470b287b7ca912d08f59cf6a3403f5e
nicholasvp <nicholasvpinheiro@gmail.com>
2022-12-02 14:16:30 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let nOne = prompt('Digite um valor')
+let nTwo = prompt('Digite outro valor')
+
+nOne = Number(nOne)
+nTwo = Number(nTwo)
+
+const som = nOne + nTwo
+const sub = nOne - nTwo
+
+alert('Soma: ' + som)
+alert('Subtração: ' + sub)
+
